6068b538cce33195df7780d61e89a43ed0c5d73f
codingsama <44747683+codingsama@users.noreply.github.com>
2018-11-04 15:02:12 +0100

    Initial commit

---
+++ app/src/main/java/com/joshcoding/beeradvisor/BeerAdvisor.java
@@ -0,0 +1,13 @@
+package com.joshcoding.beeradvisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeerAdvisor extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_advisor);
+    }
+}
